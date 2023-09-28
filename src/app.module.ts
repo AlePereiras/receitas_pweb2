cc832407d0a0a04ba6eaae935b28c1c16d932d28
AlefMcz <128554136+AlePereiras@users.noreply.github.com>
2023-09-28 14:01:33 -0300

    colocando o codigo no  app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReceitasService } from './receitas/receitas.service';
+import { ReceitasController } from './receitas/receitas.controller';
+import { ReceitasModule } from './receitas/receitas.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ReceitasModule],
+  controllers: [AppController, ReceitasController],
+  providers: [AppService, ReceitasService],
 })
 export class AppModule {}
