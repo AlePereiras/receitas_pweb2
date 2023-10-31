48e1dd1042a6693ab8eecce2b0629476c62b94a3
ale <128554136+AlePereiras@users.noreply.github.com>
2023-10-31 13:56:34 -0300

    ajustando o receitas.module

--- src/receitas/receitas.module.ts
+++ src/receitas/receitas.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ReceitasController } from './receitas.controller';
 import { ReceitasService } from './receitas.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Receita } from './receita.entity';
 
 @Module({
+    imports: [TypeOrmModule.forFeature([Receita])],
     controllers: [ReceitasController],
     providers: [ReceitasService]
 })
-export class ReceitasModule {}
+export class ReceitasModule { }
