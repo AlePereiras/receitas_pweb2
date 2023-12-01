5c792a1cae9ecbf43b74514eef0a4c8c27e61794
AlePereiras <apf1@aluno.ifal.edu.br>
2023-12-01 17:09:21 -0300

    colocando a tabela usuario no app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,12 @@ import { DataSource } from 'typeorm';
 import { Receita } from './receitas/receita.entity';
 import { Favorito } from './receitas/favoritos/favorito.entity';
 import { FavoritosModule } from './receitas/favoritos/favoritos.module';
+import { UsuariosModule } from './receitas/usuarios/usuarios.module';
+import { Usuario } from './receitas/usuarios/usuario.entity';
 
 
 @Module({
-  imports: [ReceitasModule, FavoritosModule, 
+  imports: [ReceitasModule, FavoritosModule, UsuariosModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'dpg-ckar75kiibqc73af10t0-a.oregon-postgres.render.com',
@@ -18,7 +20,7 @@ import { FavoritosModule } from './receitas/favoritos/favoritos.module';
       username: 'receitas_pweb2_user',
       password: 'AyzhFdm2Zne1g1YEP1Qyqy6YtUbixoPq',
       database: 'receitas_pweb2',
-      entities: [Receita, Favorito],
+      entities: [Receita, Favorito, Usuario],
       synchronize: true,
       ssl: true
     }),
