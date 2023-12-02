1be8d6ea3b0ffec9d741657450ec3360cc8f3274
AlePereiras <apf1@aluno.ifal.edu.br>
2023-12-02 12:17:32 -0300

    ajustando um erro bobo

--- src/receitas/receitas.module.ts
+++ src/receitas/receitas.module.ts
@@ -10,13 +10,13 @@ import { Receita } from './receita.entity';
 // import { UsuariosController } from './usuarios/usuarios.controller';
 // import { UsuariosService } from './usuarios/usuarios.service';
 // import { UsuariosModule } from './usuarios/usuarios.module';
-import { IngredientesController } from './ingredientes/ingredientes.controller';
-import { IngredientesService } from './ingredientes/ingredientes.service';
-import { IngredientesModule } from './ingredientes/ingredientes.module';
+// import { IngredientesController } from './ingredientes/ingredientes.controller';
+// import { IngredientesService } from './ingredientes/ingredientes.service';
+// import { IngredientesModule } from './ingredientes/ingredientes.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Receita]), IngredientesModule],
-    controllers: [ReceitasController, IngredientesController],
-    providers: [ReceitasService, IngredientesService]
+    imports: [TypeOrmModule.forFeature([Receita])],
+    controllers: [ReceitasController],
+    providers: [ReceitasService]
 })
 export class ReceitasModule { }
