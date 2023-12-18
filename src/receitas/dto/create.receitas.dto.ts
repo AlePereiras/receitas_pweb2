3fc7036f05fa92b03947713acf564efba4af63e4
AlePereiras <apf1@aluno.ifal.edu.br>
2023-12-18 20:37:13 -0300

    teste de dto

--- src/receitas/dto/create.receitas.dto.ts
+++ src/receitas/dto/create.receitas.dto.ts
@@ -1,8 +1,20 @@
+// import { IsString, Matches } from 'class-validator';
 export class CreateOrUpdateReceitasDto {
 
     imagem: string;
+
+    // @IsString()
     nomeReceita: string;
+
+    // @IsString()
     descricao: string;
+
+    // @IsString()
     ingredientes: string;
+
+  
+//    @Matches(/^[a-zA-Z0-9\s,.]+$/, {
+//   message: 'O campo pode conter apenas letras, números, espaços, vírgulas e pontos.',
+// })
     modoFazer: string
 }
